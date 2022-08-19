46083200d91513af9fd76bcef9e679806165a838
Jonata <jonata.a@outlook.com>
2022-08-19 20:44:45 -0300

    Create Security Configuration

---
+++ src/main/java/com/jonata/mudi/SecurityConfiguration.java
@@ -0,0 +1,38 @@
+package com.jonata.mudi;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authz -> authz
+                        .anyRequest().authenticated()
+                )
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .permitAll()
+                );
+        return http.build();
+    }
+
+        @Bean
+        public InMemoryUserDetailsManager userDetailsService() {
+            UserDetails user = User.withDefaultPasswordEncoder()
+                    .username("admin")
+                    .password("admin")
+                    .roles("ADM")
+                    .build();
+            return new InMemoryUserDetailsManager(user);
+        }
+    }
